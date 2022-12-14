9ab64ee4b90bd902c2e646b4101b47a890b5c10a
Liya Susan Biju <liyasusanbiju@gmail.com>
2022-12-15 01:22:12 +0530

    edited

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,24 +3,26 @@ import home from '../images/home.png'
 
 function About() {
   return (
+    
     <h5 style={{display: 'flex',
     justifyContent: 'center',
-    alignItems: 'center',flexDirection:'column',height:'100vh',lineHeight:'25px',}}>
-      
-      <h4><b>Delinate </b></h4> is a <h5><b>24 hour hackathon</b></h5> by Catalyst  Mar Baselios College of Engineering and Technology  IEDC <div> as a part of Innovators Premier League by Kerala start up mission.</div><div>The winners will be nominated for regional level   hackathon </div> conducted by kerala start up mission.
+    paddingBottom:'6%',color:'white',
+    alignItems: 'center',flexDirection:'column',height:'60vh',lineHeight:'25px',PaddingTop:'6%',backgroundColor:'#000033'}}>
+      <h2 style={{marginBottom:'3%'}}>ABOUT</h2>
+      <h4><b>Converge </b></h4> is a <h5><b>24 hour hackathon</b></h5> by Catalyst  Mar Baselios College of Engineering and Technology  IEDC <div> as a part of Innovators Premier League by Kerala start up mission.</div><div>The winners will be nominated for regional level   hackathon </div> conducted by kerala start up mission.
       <br/>
       <br/>
       <button style={{display: 'inlineBlock',
       margin:'0 auto',
         textDecoration: 'none',
-        background: 'red',
+        background: '#0676B8',
         color: 'white',
         padding:'20px',
         cursor: 'pointer',
-        border: '3px solid ',
+        border: '3px solid #0676B8 ',
         padding:'10px',
         borderRadius:'20px',
-        position:'relative'}}>Register Now</button>
+        position:'relative'}}>KNOW MORE</button>
     </h5>
     
   )
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import hackathon from '../images/home.png'
+import hackathon from '../images/hometwo.png'
 import navbar from '../images/navbar.png'
 
 function Home() {
   return (
-    <section className=" text-light p-5 p-lg-0 pt-lg-5 text-center text-sm-start pb-5 home" style={{ backgroundImage: `url(${hackathon})`,backgroundRepeat: 'no-repeat',
-    backgroundSize: 'cover',height:'100vh', backgroundPosition: 'center'
+    <section  style={{ backgroundImage: `url(${hackathon})`,backgroundRepeat: 'no-repeat',
+    backgroundSize: '100% 100%',height:'100vh', backgroundPosition: 'center',width:'100vw'
     }}>
        </section>
   )
