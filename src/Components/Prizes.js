f03b9d8d5a350de01d3d75ffde360fe1eedd3a53
Liya Susan Biju <liyasusanbiju@gmail.com>
2022-12-15 17:43:09 +0530

    updated

--- src/Components/Prizes.js
+++ src/Components/Prizes.js
@@ -1,22 +1,57 @@
 import React from 'react'
 import prizes from '../images/prizes.png'
+import imgtwo from '../images/imgtwo.svg'
 
 function Prizes() {
   return (
-    <div style={{backgroundColor:'#000033',color:'white'}} className="d-sm-flex align-items-center justify-content-between p-5">
-      <div><h1 >PRIZES</h1>
-      
-      
-      <h3 style={{marginTop:'40px'}}>1st prize: Rs. 6000/-</h3>
-      <h3>2nd prize: Rs. 4000/-</h3>
-      <h3 style={{marginBottom:'60px'}}>3rd prize: Rs. 3000/-</h3></div> 
-        
-      
+    
+    
+    <section class=" mt-5" id="tourist"  style={{backgroundColor:'#000033',color:'white',padding:'1%'}}>    
+     <div class="container">
+      <div class="row text-center text-align-center align-items-center">
+        <div class="col-sm-12 col-md-12 mb-4">
+            <h3 style={{margin:'50px'}} >Prizes</h3>
+         </div>
+         
+        <div class="col-md-4">
+          <div class="testimonial mb-5">
+           <div class="avatar mx-auto">
+           
+          </div>
+          <button style={{borderRadius:'50% ',padding:'20px 30px'}}> 1 </button>
+          <h4 class="font-weight-bold dark-grey-text mt-4">First Prize</h4>
+          <h6 class="font-weight-bold blue-text my-3">6000/-</h6>
+          
+        </div>
+      </div>
+ 
+      <div class="col-md-4">
+        <div class="testimonial mb-5">
+          <div class="avatar mx-auto">
+           
+          </div>
+          <button style={{borderRadius:'50% ',padding:'20px 30px'}}> 2 </button>
+          <h4 class="font-weight-bold dark-grey-text mt-4">Second Prize</h4>
+          <h6 class="font-weight-bold blue-text my-3">4000/-</h6>
+         
+        </div>
+ 
+      </div>
+      <div class="col-md-4">
+        <div class="testimonial mb-5">
+          <div class="avatar mx-auto">
+           
+          </div>
+          <button style={{borderRadius:'50% ',padding:'20px 30px'}}> 3 </button>
+          <h4 class="font-weight-bold dark-grey-text mt-4">Third Prize</h4>
+          <h6 class="font-weight-bold blue-text my-3">2000/-</h6>
+          
+        </div>
+        </div>
+      </div>
+    </div> 
+   </section> 
 
-    <img src={prizes} className="img-fluid w-50 d-none d-sm-block"/>
-
-
-  </div>
   )
 }
 
