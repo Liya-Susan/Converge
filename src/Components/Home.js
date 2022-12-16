bb4b53bc6d29c086cbc8eaf8a533af645036cd95
Liya Susan Biju <liyasusanbiju@gmail.com>
2022-12-16 23:00:16 +0530

    updated

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -7,7 +7,7 @@ import home from '../images/home.png'
 
 function Home(){
   const style={ backgroundImage: `url(${hackathon})`,backgroundRepeat: 'no-repeat',
-  backgroundSize:'100% 100%',height:'100vh', backgroundPosition: 'center',width:'98.7vw',overflowX:'hidden','@media  (max-width:400px)':{
+  backgroundSize:'100% 100%',height:'100vh', backgroundPosition: 'center',width:'100vw',overflowX:'hidden','@media  (max-width:400px)':{
     backgroundImage: `url(${home})`,height:'70vh'
   }
   }
